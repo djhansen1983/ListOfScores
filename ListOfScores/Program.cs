a10eed8d98100336e51f6ef8589b3bc3c8b2bf0a
djhansen <david.j.hansen@gmail.com>
2020-06-24 14:40:11 -0400

    Add project files.

---
+++ ListOfScores/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ListOfScores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var scores = new List<int>();
+            scores.Add(42);
+            scores.Add(98);
+            scores.Add(66);
+            scores.Add(5);
+            scores.Add(100);
+
+            var fixedScores = scores.ToArray();
+            var genericFixedScore = new List<int>(fixedScores);
+
+            var total = 0;
+            foreach(var item in scores)
+            {
+                total += item;
+            }
+        }
+    }
+}
